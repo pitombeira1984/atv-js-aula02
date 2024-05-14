27ab87fef1bb86f83cc7764cb1ad883fe9eaea1b
pitombeira1984 <168240874+pitombeira1984@users.noreply.github.com>
2024-05-14 15:56:38 -0300

    Conclusão de mais atividade

--- Aula02/dom.js
+++ Aula02/dom.js
@@ -40,3 +40,85 @@ else{
     console.log(`O cliente tem ${idade} anos, e comprou um produto por R$${pc}, então não tem desconto de 10%`)
 }
 // ---Atividade 03---
+// ---Atividade 04---
+var letra = prompt("Digite uma letra:")
+let vogal = ["a", "e", "i", "o", "u"]
+let vogalm = ["A", "E", "I", "O", "U"]
+if (letra === vogal[0] || letra === vogalm[0]){
+    console.log(`A letra digitada "${letra}" é uma Vogal `)
+}
+else if (letra === vogal[1] || letra === vogalm[1]){
+    console.log(`A letra digitada "${letra}" é uma Vogal `)
+}
+else if (letra === vogal[2] || letra === vogalm[2]){
+    console.log(`A letra digitada "${letra}" é uma Vogal `)
+}
+else if (letra === vogal[3] || letra === vogalm[3]){
+    console.log(`A letra digitada "${letra}" é uma Vogal `)
+}
+else if (letra === vogal[4] || letra === vogalm[4]){
+    console.log(`A letra digitada "${letra}" é uma Vogal `)
+}
+else{
+    console.log(`A letra digitada "${letra}" é uma Consoante `)
+}
+// ---Atividade 04---
+// ---Atividade 05---
+var semana = Number(prompt("Digite um número de 1 a 7"))
+switch(semana){
+    case 1:console.log(`O numero "${semana}" equivale a segunda-feira `)
+    break
+    case 2:console.log(`O numero "${semana}" equivale a terça-feira `)
+    break
+    case 3:console.log(`O numero "${semana}" equivale a quarta-feira `)
+    break
+    case 4:console.log(`O numero "${semana}" equivale a quinta-feira `)
+    break
+    case 5:console.log(`O numero "${semana}" equivale a sexta-feira `)
+    break
+    case 6:console.log(`O numero "${semana}" equivale a sábado `)
+    break
+    case 7:console.log(`O numero "${semana}" equivale a domingo `)
+    break
+    default: console.log("Nenhuma das Alternatival")
+    
+}
+// ---Atividade 05---
+// ---Atividade 06---
+
+   // ---Não Consegui fazer a Atividade 06---
+   
+// ---Atividade 06---
+// ---Desafio Prático---
+
+console.log("Digite o Calculo que deseja executar:") 
+console.log("Digite 1 para Área do Triangulo:")
+console.log("Digite 2 para Área do Retangulo:")
+console.log("Digite 3 para Volume do Cubo:")
+console.log("Digite 4 para Área do Circulo:")
+
+var cal = Number(prompt("Digite o Calculo:"))
+
+if (cal == 1){
+    var b = prompt("Digite a Base:")
+    var a = prompt("Digite a Altura:")
+    console.log(`O numero digitado é ${cal}, então a Área do Triângulo é ${(b*a)/2}`)
+}
+else if (cal == 2){
+    var b = prompt("Digite a Base:")
+    var a = prompt("Digite a Altura:")
+    console.log(`O numero digitado é ${cal}, então a Área do Retângulo é ${(b*a)}`)
+}
+else if (cal == 3){
+    var aresta = prompt("Digite a Aresta:")
+    console.log(`O numero digitado é ${cal}, então o Volume do Cubo é ${(aresta**3)}`)
+}
+else if (cal == 4){
+    const pi = 3.14
+    var r = prompt("Digite o Raio:")
+    console.log(`O numero digitado é ${cal}, então a Área do Circulo é ${(pi*(r**2))}`)
+}
+else{
+    console.log("Nenhuma Opção foi Digitada")
+}
+// ---Desafio Prático---
